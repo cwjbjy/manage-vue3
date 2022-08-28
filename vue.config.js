2ee85d9790f5c08dedb7b8e465db00d2d18130a8
cwj <595842570@qq.com>
2022-08-28 21:33:25 +0800

    feat: 打包配置 (story#000)

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,54 @@
 const { defineConfig } = require('@vue/cli-service');
+const AutoImport = require('unplugin-auto-import/webpack');
+const Components = require('unplugin-vue-components/webpack');
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: process.env.NODE_ENV === 'production' ? false : true,
-  configureWebpack: {
-    plugins: [],
-    resolve: {
-      fallback: { path: require.resolve('path-browserify') },
-    },
-  },
   css: {
     loaderOptions: {
       scss: {
         additionalData: `@import "@/styles/index.scss";`,
         //旧版本用prependData，新版比如cli5本用additionalData
+      },
+    },
+  },
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      fallback: { path: require.resolve('path-browserify') },
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        name: 'vendor',
+        cacheGroups: {
+          'vue-router': {
+            name: 'vue-router',
+            test: /[\\/]node_modules[\\/]vue-router[\\/]/,
+            chunks: 'all',
+          },
+          'element-plus': {
+            name: 'element-plus',
+            test: /[\\/]node_modules[\\/]element-plus[\\/]/,
+            priority: 40,
+            chunks: 'all',
+          },
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minChunks: 2,
+            priority: 20,
+          },
+        },
       },
     },
   },
